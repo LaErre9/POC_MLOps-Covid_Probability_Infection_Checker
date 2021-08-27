baae29797ceea5e93efbda72a55c2bce4834a086
Antonio Romano <antonio.romano45@studenti.unina.it>
2021-08-27 18:15:42 +0200

    richange model - main

--- myTraining.py
+++ myTraining.py
@@ -51,7 +51,7 @@ if __name__== "__main__":
     Y_test = test[['COVID-19']].to_numpy().reshape(1086,)
 
    # ------------ LogisticRegression ---------------------------------------------
-    clf = LogisticRegression()
+    clf = DecisionTreeClassifier()
     clf.fit(X_train, Y_train)
 
     #Score/Accuracy
