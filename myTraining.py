364e2774422d00708401362456f987773712ea09
unknown <antonio.romano45@studenti.unina.it>
2021-09-01 18:51:35 +0200

    che succede alla web app?

--- myTraining.py
+++ myTraining.py
@@ -54,7 +54,6 @@ if __name__== "__main__":
     Y_test = test[['COVID-19']].to_numpy().reshape(1086,)
 
    # ------------ LogisticRegression ---------------------------------------------
-    
     clf = LogisticRegression()
     clf.fit(X_train, Y_train)
 
